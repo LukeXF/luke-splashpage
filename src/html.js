42f9596bf224dff64afd370d745d2ffcd25c4cdf
Luke Brown <me@luke.sx>
2020-08-06 18:51:23 +0100

    MP-247 SEO update

--- src/html.js
+++ src/html.js
@@ -6,21 +6,25 @@ export default class HTML extends React.Component {
         const description = 'Full Stack JavaScript Developer and Business Owner in London/Ipswich, England.';
         const url = 'https://luke.sx';
         const twitterUsername = 'lukenxf';
+        const image = 'https://luke.sx/images/preview.png';
         return (
             <html {...this.props.htmlAttributes} lang="en">
             <head>
                 <title>{title}</title>
                 <meta name="description" content={description}/>
+
+                <meta property="og:type" content="website"/>
                 <meta property="og:url" content={url}/>
                 <meta property="og:title" content={title}/>
                 <meta property="og:description" content={description}/>
+                <meta property="og:image" content={image}/>
 
-                <meta property="og:image" content={'https://luke.sx/preview.png'}
-                />
+                <meta name="twitter:url" content={url}/>
                 <meta name="twitter:card" content="summary_large_image"/>
                 {twitterUsername && <meta name="twitter:creator" content={twitterUsername}/>}
                 <meta name="twitter:title" content={title}/>
                 <meta name="twitter:description" content={description}/>
+                <meta name="twitter:image" content={image}/>
 
                 <meta charSet="utf-8"/>
                 <meta httpEquiv="x-ua-compatible" content="ie=edge"/>
