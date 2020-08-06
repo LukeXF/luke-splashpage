96de7150f180d3904396cc799d62d026fa9d96a2
Luke Brown <me@luke.sx>
2020-08-06 18:41:58 +0100

    MP-247 Finished setup for Luke.sx

--- src/html.js
+++ src/html.js
@@ -2,59 +2,56 @@ import React from 'react';
 
 export default class HTML extends React.Component {
     render() {
+        const title = 'Luke J Brown';
+        const description = 'Full Stack JavaScript Developer and Business Owner in London/Ipswich, England.';
+        const url = 'https://luke.sx';
+        const twitterUsername = 'lukenxf';
         return (
             <html {...this.props.htmlAttributes} lang="en">
-                <head>
-                    <meta charSet="utf-8" />
-                    <meta httpEquiv="x-ua-compatible" content="ie=edge" />
-                    <meta
-                        name="viewport"
-                        content="width=device-width, initial-scale=1, shrink-to-fit=no"
-                    />
-                    <script src="https://identity.netlify.com/v1/netlify-identity-widget.js" />
-                    <script src="/admin/netlify_redirect.js" />
-                    {this.props.headComponents}
+            <head>
+                <title>{title}</title>
+                <meta name="description" content={description}/>
+                <meta property="og:url" content={url}/>
+                <meta property="og:title" content={title}/>
+                <meta property="og:description" content={description}/>
 
-                    <link
-                        rel="apple-touch-icon"
-                        sizes="180x180"
-                        href="/static/images/favicon/apple-touch-icon.png"
-                    />
-                    <link
-                        rel="icon"
-                        type="image/png"
-                        sizes="32x32"
-                        href="/static/images/favicon/favicon-32x32.png"
-                    />
-                    <link
-                        rel="icon"
-                        type="image/png"
-                        sizes="16x16"
-                        href="/static/images/favicon/favicon-16x16.png"
-                    />
-                    <link rel="manifest" href="/static/images/favicon/site.webmanifest" />
-                    <link
-                        rel="mask-icon"
-                        href="/static/images/favicon/safari-pinned-tab.svg"
-                        color="#3d5e67"
-                    />
-                    <link rel="shortcut icon" href="/static/images/favicon/favicon.ico" />
-                    <meta name="msapplication-TileColor" content="#3d5e67" />
-                    <meta
-                        name="msapplication-config"
-                        content="/static/images/favicon/browserconfig.xml"
-                    />
-                    <meta name="theme-color" content="#ffffff" />
-                </head>
-                <body {...this.props.bodyAttributes}>
-                    {this.props.preBodyComponents}
-                    <div
-                        key={`body`}
-                        id="___gatsby"
-                        dangerouslySetInnerHTML={{ __html: this.props.body }}
-                    />
-                    {this.props.postBodyComponents}
-                </body>
+                <meta property="og:image" content={'https://luke.sx/preview.png'}
+                />
+                <meta name="twitter:card" content="summary_large_image"/>
+                {twitterUsername && <meta name="twitter:creator" content={twitterUsername}/>}
+                <meta name="twitter:title" content={title}/>
+                <meta name="twitter:description" content={description}/>
+
+                <meta charSet="utf-8"/>
+                <meta httpEquiv="x-ua-compatible" content="ie=edge"/>
+                <meta
+                    name="viewport"
+                    content="width=device-width, initial-scale=1, shrink-to-fit=no"
+                />
+                <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"/>
+                <script src="/admin/netlify_redirect.js"/>
+
+                {this.props.headComponents}
+
+                <link rel="apple-touch-icon" sizes="180x180" href="/images/favicon/apple-touch-icon.png"/>
+                <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon/favicon-32x32.png"/>
+                <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon/favicon-16x16.png"/>
+                <link rel="manifest" href="/images/favicon/site.webmanifest"/>
+                <link rel="mask-icon" href="/images/favicon/safari-pinned-tab.svg" color="#d29600"/>
+                <link rel="shortcut icon" href="/images/favicon/favicon.ico"/>
+                <meta name="msapplication-TileColor" content="#d29600"/>
+                <meta name="msapplication-config" content="/images/favicon/browserconfig.xml"/>
+                <meta name="theme-color" content="#d29600"/>
+            </head>
+            <body {...this.props.bodyAttributes}>
+            {this.props.preBodyComponents}
+            <div
+                key={`body`}
+                id="___gatsby"
+                dangerouslySetInnerHTML={{ __html: this.props.body }}
+            />
+            {this.props.postBodyComponents}
+            </body>
             </html>
         );
     }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React, { Fragment } from 'react';
 import '@styles/styles.scss';
 import styles from './styles.module.scss';
-import Logo from '@images/logo.svg';
+// import Logo from '@images/luke.png';
 
 const Home = () => {
     return (
@@ -9,15 +9,42 @@ const Home = () => {
             <div className={styles.offline}>
                 <div className={styles.offlineContent}>
                     <div className={styles.offlineLogo}>
-                        <Logo />
+                        <img src="images/luke.png"/>
                     </div>
-                    <h1>Sorry, this website is offline right now.</h1>
-                    <p>Contact the system administrator at <a href="mailto:support@merlinpanel.com">support@merlinpanel.com</a> for more information.</p>
+                    <h1>Luke J Brown</h1>
+                    <h3>
+                        CEO at <a href="https://elementsoftworks.co.uk">Element Softworks Ltd</a> //
+                        CTO at <a href="https://skippermyboat.com">Skipper My Boat Inc.</a></h3>
+                    <p>
+                        Since 2012 I have had a passion for development and digital creativity. After several years of
+                        freelancing work I setup my company, Element Softworks Ltd to grow a group of highly-talented
+                        specialists to form a reliable full-service creative agency for small to medium businesses in
+                        the UK and the USA.
+                    </p>
+                    <p>
+                        We pride ourselves on building bespoke, responsive and innovative solutions for clients in
+                        all industries. To learn more please view <a href="https://elementsoftworks.co.uk/brochure">our
+                        brochure</a>. If you're interested in hiring me or my services then we charge £300-£400 per day
+                        per person. We specialise in React.Js, Node.js, No SQL architecture.
+                    </p>
+                    <p>In the most recent years I have joined Skipper My Boat as the CTO to build a peer-to-peer global
+                        yacht rental platform, as well as setting up a SaaS company
+                        called <a href="https://merlinpanel.com">Merlin Panel</a> to empower businesses to control their
+                        online presence all in one place.</p>
+
+                    <div className={styles.signature}>
+                        <img src="images/lb-signature-dark.png"/>
+                    </div>
                 </div>
-                <footer><p>This website was created by <a href="https://elementsoftworks.co.uk">Element Softworks Ltd.</a></p></footer>
+                <footer>
+                    <p>
+                        This website was created by <a href="https://elementsoftworks.co.uk">Element Softworks
+                        Ltd</a> and is powered by <a href="https://merlinpanel.com">Merlin Panel</a>.
+                    </p>
+                </footer>
             </div>
         </Fragment>
-    )
+    );
 };
 
 export default Home;
